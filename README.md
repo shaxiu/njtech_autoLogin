6c023a77dd915230936483b4ecff37faee2db7a1
shaxiu <coderxiu@qq.com>
2022-10-10 16:56:47 +0800

    提交v1.0代码

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# njtech_autoLogin
-南京工业大学无线网自动登录(支持识别验证码)
+# 南京工业大学无线网自动登录脚本
+
+🎈脚本主要用于自动连接学校无线网 `Njtech-home` `Njtech`
+
+- [x] 支持 `Njtch` `Njtech-home`
+
+- [x] 支持验证码识别
+
+### 环境配置
+
+- Python3
+
+- selenium
+
+- ddddocr
+
+- configparser
+
+- chromedriver
+
+- chrome
+
+### 安装
+
+默认已安装好python3 以及 chrome
+
+#### 安装相应库
+
+`pip install selenium`
+
+`pip install ddddocr`
+
+`pip install configparser`
+
+#### 下载chromedriver
+
+根据自己电脑上的chrome版本下载对应版本的驱动
+
+
+| [**ChromeDriver 107.0.5304.18**](https://registry.npmmirror.com/binary.html?path=chromedriver/107.0.5304.18/) | [**ChromeDriver 106.0.5249.61**](https://registry.npmmirror.com/binary.html?path=chromedriver/106.0.5249.61/) | [**ChromeDriver 106.0.5249.21**](https://chromedriver.storage.googleapis.com/index.html?path=106.0.5249.21/) |
+| ------------------------------------------------------------ | ------------------------------------------------------------ | ------------------------------------------------------------ |
+| [**ChromeDriver 105.0.5195.52**](https://chromedriver.storage.googleapis.com/index.html?path=105.0.5195.52) | [**ChromeDriver 105.0.5195.19**](https://chromedriver.storage.googleapis.com/index.html?path=105.0.5195.19/) | [**ChromeDriver 104.0.5112.79**](https://chromedriver.storage.googleapis.com/index.html?path=104.0.5112.79/) |           
+
+
+  更多版本请访问：http://npm.taobao.org/mirrors/chromedriver/
+
+## 脚本配置
+
+- **将代码中的`chromedriver路径`替换为已下载到本地的路径/替换项目中的Chromedriver.exe**
+
+  `driver=webdriver.Chrome(executable_path="chromedriver相对路径", options=option)`
+
+- **修改`config.ini`**
+
+  ```ini
+  [accountInfo]
+  account = 学号
+  password = 密码
+  channel= 1
+  headless=false
+  ```
+
+  ```ini
+  [附加说明]
+  ------------------
+  channel=0 校园网
+  channel=1 中国移动
+  channel=2 中国电信
+  ------------------
+  headless=true 无GUI 适用于服务器或自动运行脚本
+  headless=false 有GUI 可以看到登录情况
+  ```
+
+- 理论上代码适配全平台,`MAC` `Linux` 用户可自行调整代码，不单独进行打包，现已将适配 Windows 的版本 打包成`exe`发布
---
+++ main.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import ddddocr
+import configparser
+
+
+def main():
+    channelsName=['校园网','中国移动','中国电信']
+    option = webdriver.ChromeOptions()
+    if user_headless=='true':
+        option.add_argument('--headless')
+    option.add_experimental_option(
+        "excludeSwitches", ['enable-automation', 'enable-logging'])
+    option.add_argument(
+        'user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"')
+    driver = webdriver.Chrome(
+        executable_path="./chromedriver.exe", options=option)
+    driver.get('http://i.njtech.edu.cn/')
+    time.sleep(2)
+    img = driver.find_element(By.ID,"pc-captcha")
+    image_bytes = img.screenshot_as_png
+    captchaCode = ocr.classification(image_bytes)
+    accountInput = driver.find_element(By.ID,"username")
+    passwdInput = driver.find_element(By.ID,"password")
+    captchaInput = driver.find_element(By.ID,'imgcaptcha')
+    longinButton = driver.find_element(By.ID,'login')
+    accountInput.send_keys(user_account)
+    passwdInput.send_keys(user_password)
+    captchaInput.send_keys(captchaCode)
+    try:
+        # 通道选择
+        channelSelect = driver.find_element_by_id('channelshow')
+        channels = driver.find_elements_by_tag_name('span')[user_channel]
+        channelSelect.click()
+        channels.click()
+        print('正在连接 njtech-home ...')
+        print('服务商为:'+channelsName[user_channel])
+    except:
+        print('正在连接 njtech ...')
+    longinButton.click()
+    print('执行成功')
+    driver.quit()
+
+
+if __name__ == '__main__':
+    print('-----------------------------')
+    print('南京工业大学无线网登陆脚本')
+    print('designed by coderxiu')
+    print('Github地址:https://github.com/shaxiu/njtech_autoLogin')
+    print('-----------------------------')
+    print('正在读取初始化配置文件')
+    # 导入模型文件
+    ocr = ddddocr.DdddOcr(det=False, ocr=False, import_onnx_path="./models/captcha_1.0_3053_19000_2022-10-09-13-35-56.onnx",
+                          charsets_path="./models/charsets.json",show_ad=False)
+    # 读取配置文件
+    CONF_FILE = './config.ini'
+    cf = configparser.ConfigParser()
+    cf.read(CONF_FILE)
+    user_account = cf.get("accountInfo", "account")
+    user_password = cf.get("accountInfo", "password")
+    user_channel = int(cf.get("accountInfo", "channel")) if int(cf.get("accountInfo", "channel")) in [0,1,2] else 0
+    user_headless = cf.get("accountInfo", "headless") if cf.get("accountInfo", "headless") in ['true','false'] else 'true'
+    main()
